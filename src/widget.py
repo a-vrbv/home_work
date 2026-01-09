49ca4c7959df9b68e295d701e299bff64b91cc1e
Андрей Воробьев <dezertir-90@mail.ru>
2026-01-09 10:59:40 +0600

    доработал функцию mask_account_card маскировкой из модуля masks.py

--- src/widget.py
+++ src/widget.py
@@ -14,15 +14,13 @@ def mask_account_card(mask_number: str) -> str:
 
     if "Счет" in mask_number or "Счёт" in mask_number:
         """Проверяем содержит ли строку 'Счет'.
-        Для счета показываем последние 4 цифры, остальное маскируем"""
-        hidden_element = '*' * (len(number) - 4)
-        hidden_number = f"{hidden_element}{number[-4:]}"
+        Для счета показываем последние 4 цифры"""
+        hidden_number = f"**{number[-4:]}"
         return f"Счет {hidden_number}"
     else:
         """Для карты показываем первые 6 цифр и последние 4 цифры,
         середину маскируем."""
-        middle_element = '*' * (len(number) - 10)
-        hidden_middle_element = f"{number[:6]}{middle_element}{number[-4:]}"
+        hidden_middle_element = f"{number[:4]} {number[4:6]}** **** {number[-4:]}"
 
         """Берем название карты и соединяем со скрытым номером."""
         cart_name = " ".join(elements[:-1])
