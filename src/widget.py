f39ee0ef2d8d7170764f5fdf5f18cdbc61ef6746
Андрей Воробьев <dezertir-90@mail.ru>
2026-01-10 15:19:51 +0600

    импорт функций

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,37 @@
+from masks import get_mask_card_number, get_mask_account
+
+def get_mask_card_number(cart_number: int | str) -> str:
+    """Функция принимает на вход номер карты и возвращает ее маску.
+    Номер карты замаскирован и отображается в формате
+    XXXX XX** **** XXXX"""
+    cart_str = str(cart_number)
+    """Преобразовываем в строку полученные значения"""
+
+    if len(cart_str) != 16:
+        """Проверяем длину номера карты. Должно быть 16 цифр"""
+        raise ValueError("Номер карты состоит из 16 цифр.")
+
+    masked_number = f"{cart_str[:4]} {cart_str[4:6]}** ****{cart_str[-4:]}"
+    """Преобразовываем номер карты в маску."""
+    return masked_number
+
+
+def get_mask_account(account_number: int | str) -> str:
+    """Функция принимает на вход номер счета и возвращает его маску.
+    Номер счета замаскирован и отображается в формате **XXXX"""
+
+    account_str = str(account_number)
+
+    if len(account_str) < 4:
+        """Проверяем длину номера счета. Должно быть не менее 4 цифр"""
+        raise ValueError("Номер счета содержит не менее 4 цифр.")
+
+    masked_account_number = f"**{account_str[-4:]}"
+    """Преобразовываем номер счета в маску."""
+    return masked_account_number
+
+
+
 def mask_account_card(mask_number: str) -> str:
     """Функция принимает аргумент - строку, содержащую тип и номер карты или счета.
     Аргументом может быть строка типа
