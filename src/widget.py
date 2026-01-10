a7c43c5da27753b1acd863f70b3ed27631cd86a5
Андрей Воробьев <dezertir-90@mail.ru>
2026-01-10 14:58:38 +0600

    сщхранил изменения в widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,3 @@
-
-
 def mask_account_card(mask_number: str) -> str:
     """Функция принимает аргумент - строку, содержащую тип и номер карты или счета.
     Аргументом может быть строка типа
@@ -21,7 +19,6 @@ def mask_account_card(mask_number: str) -> str:
         """Для карты показываем первые 6 цифр и последние 4 цифры,
         середину маскируем."""
         hidden_middle_element = f"{number[:4]} {number[4:6]}** **** {number[-4:]}"
-
         """Берем название карты и соединяем со скрытым номером."""
         cart_name = " ".join(elements[:-1])
         return f"{cart_name} {hidden_middle_element}"
@@ -29,7 +26,7 @@ def mask_account_card(mask_number: str) -> str:
 
 def get_date(date_today: str) -> str:
     """Получаем строку с датой в формате "2024-03-11T02:26:18.671407"
-       и возвращает строку с датой в формате "ДД.ММ.ГГГГ"."""
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ"."""
 
     day = date_today[8:10]
     month = date_today[5:7]
